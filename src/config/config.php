9af12257b7d0903b51c1b0a85a09573f4a7cb174
Alex Fiorello <alex.fiorello@global4.co.uk>
2016-01-22 10:40:01 +0000

    Added configiguration file

---
+++ src/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    return array(
+    'affinityWSDL'      =>  "https://api.affinity.akjl.co.uk/[APIuser]/WSDL/AffinityAPIService.WSDL",
+    'affinityUserName'  =>  "[AffinityUsername]",
+    'affinityPassword'  =>  "[AffinityPassword]",
+    'affinityAppName'   =>  "[AppName]",
+    'affinityAppRef'    =>  "[LaravelAPI]"
+);
